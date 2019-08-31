7b07b5a882819ea92e860907a30f6c19b1436010
木兰 <13718047371m@sina.cn>
2019-08-31 13:22:41 +0800

    整理目录-准备资源

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -8,9 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: Login
     },
+
     {
       path: '/about',
       name: 'about',
