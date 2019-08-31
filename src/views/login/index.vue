a4830d194624f14fba921f3f6a0b89e1826cec4e
木兰 <13718047371m@sina.cn>
2019-08-31 19:52:34 +0800

    登陆页面静态效果完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,22 @@
 <template>
 <div class="login">
-    <el-card class="box-card" style="width:350px ;height:350px">
+    <el-card class="box-card" style="width:400px ;height:350px">
         <img src="../../assets/logo_index.png" alt="">
         <el-form>
-            <el-form-item>
-                <el-input></el-input>
-
+            <el-form-item >
+                <el-input placeholder="请输入手机号" ></el-input>
             </el-form-item>
+            <el-form-item >
+                <el-input placeholder="验证码" style="width:240px" ></el-input>
+                <el-button style="float:right">发送验证码</el-button>
+            </el-form-item>
+            <el-form-item >
+                 <el-checkbox :checked="true" style="float:left">我已阅读并同意用户协议和隐私条款</el-checkbox>
+            </el-form-item>
+            <el-form-item>
+                 <el-button type="primary" style="width:100%">主要按钮</el-button>
+            </el-form-item>
+
         </el-form>
     </el-card>
 
